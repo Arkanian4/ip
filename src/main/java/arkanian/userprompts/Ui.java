177344c3e092b4024ae9a4df92fd2383dc760349
Darryl Y <darryl.aywz@gmail.com>
2026-02-08 01:41:02 +0800

    style-fix: Fix switch-case indentation

--- src/main/java/arkanian/userprompts/Ui.java
+++ src/main/java/arkanian/userprompts/Ui.java
@@ -37,86 +37,86 @@ public class Ui {
         boolean cont_convo = true;
         try {
             switch (instr) {
-                case "bye":
-                    message = "Bye! Hope to see you again :)\n";
-                    cont_convo = false;
-                    break;
+            case "bye":
+                message = "Bye! Hope to see you again :)\n";
+                cont_convo = false;
+                break;
 
-                case "list":
-                    message = task_list.toString();
-                    break;
+            case "list":
+                message = task_list.toString();
+                break;
 
-                case "mark":
-                    idx = parsed_input.getIdx() - 1;
-                    task = task_list.getTask(idx);
-                    task.setDone();
-                    message = "Nice! I've marked this task as done:\n"
-                            + task
-                            + "\n";
-                    Save.saveData(task_list);
-                    break;
+            case "mark":
+                idx = parsed_input.getIdx() - 1;
+                task = task_list.getTask(idx);
+                task.setDone();
+                message = "Nice! I've marked this task as done:\n"
+                        + task
+                        + "\n";
+                Save.saveData(task_list);
+                break;
 
-                case "unmark":
-                    idx = parsed_input.getIdx() - 1;
-                    task = task_list.getTask(idx);
-                    task.setNotDone();
-                    message = "OK, I've marked this task as not done yet:\n"
-                            + task
-                            + "\n";
-                    Save.saveData(task_list);
-                    break;
+            case "unmark":
+                idx = parsed_input.getIdx() - 1;
+                task = task_list.getTask(idx);
+                task.setNotDone();
+                message = "OK, I've marked this task as not done yet:\n"
+                        + task
+                        + "\n";
+                Save.saveData(task_list);
+                break;
 
-                case "delete":
-                    idx = parsed_input.getIdx() - 1;
-                    task = task_list.getTask(idx);
-                    task_list.delete(idx);
-                    message = "Noted. I've removed this task:\n"
-                            + task
-                            + "\nNow you have "
-                            + task_list.getTaskCount()
-                            + " tasks in the list."
-                            + "\n";
-                    Save.saveData(task_list);
-                    break;
+            case "delete":
+                idx = parsed_input.getIdx() - 1;
+                task = task_list.getTask(idx);
+                task_list.delete(idx);
+                message = "Noted. I've removed this task:\n"
+                        + task
+                        + "\nNow you have "
+                        + task_list.getTaskCount()
+                        + " tasks in the list."
+                        + "\n";
+                Save.saveData(task_list);
+                break;
 
-                case "todo":
-                    task = new ToDos(raw_input);
-                    task_list.addTask(task);
-                    message = "Got it. I've added this task:\n"
-                            + task
-                            + "\nNow you have "
-                            + task_list.getTaskCount()
-                            + " tasks in the list."
-                            + "\n";
-                    Save.saveData(task_list);
-                    break;
+            case "todo":
+                task = new ToDos(raw_input);
+                task_list.addTask(task);
+                message = "Got it. I've added this task:\n"
+                        + task
+                        + "\nNow you have "
+                        + task_list.getTaskCount()
+                        + " tasks in the list."
+                        + "\n";
+                Save.saveData(task_list);
+                break;
 
-                case "event":
-                    task = new Events(raw_input);
-                    task_list.addTask(task);
-                    message = "Got it. I've added this task:\n"
-                            + task
-                            + "\nNow you have "
-                            + task_list.getTaskCount()
-                            + " tasks in the list."
-                            + "\n";
-                    Save.saveData(task_list);
-                    break;
+            case "event":
+                task = new Events(raw_input);
+                task_list.addTask(task);
+                message = "Got it. I've added this task:\n"
+                        + task
+                        + "\nNow you have "
+                        + task_list.getTaskCount()
+                        + " tasks in the list."
+                        + "\n";
+                Save.saveData(task_list);
+                break;
 
-                case "deadline":
-                    task = new Deadlines(raw_input);
-                    task_list.addTask(task);
-                    message = "Got it. I've added this task:\n"
-                            + task
-                            + "\nNow you have "
-                            + task_list.getTaskCount()
-                            + " tasks in the list."
-                            + "\n";
-                    Save.saveData(task_list);
-                    break;
+            case "deadline":
+                task = new Deadlines(raw_input);
+                task_list.addTask(task);
+                message = "Got it. I've added this task:\n"
+                        + task
+                        + "\nNow you have "
+                        + task_list.getTaskCount()
+                        + " tasks in the list."
+                        + "\n";
+                Save.saveData(task_list);
+                break;
 
-                default:
-                    throw new UnknownInputException("what nonsense r u saying bruh");
+            default:
+                throw new UnknownInputException("what nonsense r u saying bruh");
             }
         } catch (InvalidTaskFormatException | UnknownInputException e) {
             message = e.getMessage();
